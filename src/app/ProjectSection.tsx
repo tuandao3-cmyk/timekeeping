e8d9eb53596e0469e33b949f4212d0629e3110fd
Bang Vu <lenhkinhsa99@gmail.com>
2024-10-15 18:58:13 +0900

    upd

--- src/app/ProjectSection.tsx
+++ src/app/ProjectSection.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
 import styles from './ProjectSection.module.css';
 
 const ProjectSection = () => {
   return (
-        <>            <section className={styles.showcaseSection}>
+        <>           
+        <section className={styles.showcaseSection}>
         <h2 className={styles.title}>Dự án đang gọi vốn</h2>
         <div className={styles.projectCards}>
           <div className={styles.projectCard}>
-            <img src="/img/19.jpg" alt="Hyperas Chain" className={styles.projectImage} />
+            <Link href="/productdetail"><img src="/img/19.jpg" alt="Hyperas Chain" className={styles.projectImage} /></Link>
             <div className={styles.badge}>Series A</div>
             <h3 className={styles.projectName}>Hyperas Chain</h3>
             <div className={styles.projectInfo}>
@@ -22,7 +24,7 @@ const ProjectSection = () => {
             </div>
           </div>
           <div className={styles.projectCard}>
-            <img src="/img/18.jpg" alt="Hyperas Chain" className={styles.projectImage} />
+            <Link href="/productdetail"><img src="/img/18.jpg" alt="Hyperas Chain" className={styles.projectImage} /></Link>
             <div className={styles.badge}>Series B</div>
             <h3 className={styles.projectName}>Salala AI</h3>
             <div className={styles.projectInfo}>
@@ -38,7 +40,7 @@ const ProjectSection = () => {
             </div>
           </div>
           <div className={styles.projectCard}>
-            <img src="/img/16.jpg" alt="Hyperas Chain" className={styles.projectImage} />
+            <Link href="/productdetail"><img src="/img/16.jpg" alt="Hyperas Chain" className={styles.projectImage} /></Link>
             <div className={styles.badge}>Series A</div>
             <h3 className={styles.projectName}>Rapital Bank</h3>
             <div className={styles.projectInfo}>
@@ -54,7 +56,7 @@ const ProjectSection = () => {
             </div>
           </div>
           <div className={styles.projectCard}>
-            <img src="/img/17.jpg" alt="Hyperas Chain" className={styles.projectImage} />
+            <Link href="/productdetail"><img src="/img/17.jpg" alt="Hyperas Chain" className={styles.projectImage} /></Link>
             <div className={styles.badge}>Series A</div>
             <h3 className={styles.projectName}>Egabid</h3>
             <div className={styles.projectInfo}>
@@ -74,7 +76,7 @@ const ProjectSection = () => {
         <button className={styles.moreProjectsButton}>TÌM HIỂU THÊM CÁC DỰ ÁN</button>
       </section>
 
-             <section className={styles.projectSection}>
+     <section className={styles.projectSection}>
                 <h2 className={styles.sectionTitle}>Dự án đã đầu tư & ươm tạo</h2>
                 <p className={styles.sectionSubtitle}>Dành cho bạn những dự án đầu tư tốt nhất</p>
                 <div className={styles.projectGrid}>
