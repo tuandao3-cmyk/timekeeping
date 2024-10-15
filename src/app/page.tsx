4ca2c54c49c731218f40149fb612c1990aaf3c56
huynq1705 <huynq1705@gmail.com>
2024-10-15 16:47:47 +0700

    add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import Head from 'next/head';
-import Header from '@/app/Header';
-import styles from './page.module.css';
-import Partner from "@/app/Partner";
+import React from "react";
+import Head from "next/head";
+import Header from "@/app/Header";
+import styles from "./page.module.css";
+import Partner from "@/app/partner";
 import ProjectSection from "@/app/ProjectSection";
 import PressSection from "@/app/PressSection";
 import ArticlesSection from "@/app/ArticlesSection";
 import Footer from "@/app/Footer";
-import Download from './download';
-import News from './news';
+import Download from "./download";
+import News from "./news";
 const Home: React.FC = () => {
     return (
         <div className={styles.container}>
             <Head>
                 <title>HYRA CAPITAL - Quỹ đầu tư Việt Nam</title>
             </Head>
-            <Header/>
-            <Partner/>
-            <ProjectSection/>
-            <News/>
-            <PressSection/>
-            <Download/>
-            <Footer/>
-
-
+            <Header />
+            <Partner />
+            <ProjectSection />
+            <News />
+            <PressSection />
+            <Download />
+            <Footer />
         </div>
     );
 };
