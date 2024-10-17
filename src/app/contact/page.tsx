9e87561e83863619f629819c67a383a920ad1c32
huynq1705 <huynq1705@gmail.com>
2024-10-17 10:31:27 +0700

    fix minor

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,25 +10,18 @@ import ContactInfo from "@/components/contact_information/ContactInfo";
 const Map = dynamic(() => import("./Map"), { ssr: false });
 
 const ContactPage: React.FC = () => {
-  return (
-    <div className="container mx-auto my-10">
-      <h1 className="text-3xl font-bold text-center mb-10">
-        Liên hệ với chúng tôi
-      </h1>
-      <div className="sm:grid-cols-1 grid lg:grid-cols-2 gap-4">
-        {/* Left box: Contact Form */}
-
-        <ContactForm />
-
-        {/* Right box: Contact Information */}
-        {/* <div className="flex-1 p-8 border rounded-lg shadow-lg max-w-md h-full"> */}
-        <div className="flex-1 contain-center">
-          <ContactInfo />
+    return (
+        <div className="container mx-auto my-10 px-11">
+            <h1 className="text-4xl font-bold text-start mb-10">
+                Liên hệ với chúng tôi
+            </h1>
+            <div className="sm:grid-cols-1 grid lg:grid-cols-2 gap-4">
+                <ContactForm />
+                <div className="flex-1 contain-center">
+                    <ContactInfo />
+                </div>
+            </div>
         </div>
-        {/* Embed the map inside ContactInfo */}
-        {/* </div> */}
-      </div>
-    </div>
-  );
+    );
 };
 export default ContactPage;
