b20c360447c1b3b27f5fd397920c84fd0efb44ca
mailyhai <mailyhai814@gmal.com>
2024-10-25 10:01:45 +0700

    fix homepage

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -63,113 +63,119 @@ const NewsPage: React.FC = () => {
         </div>
       </div>
 
-      <section className="w-full">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 p-8">
-          Sự kiện nổi bật
-        </h1>
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:grid-rows-2 gap-4 p-8 md:max-h-[600px]">
-          {newsData.map((news: any, index: number) => (
-            <>
-              {index === 1 ? (
-                <ProjectCard
-                  key={index}
-                  project={news}
-                  className="lg:col-span-2 lg:row-span-2 md:col-span-1 md:row-span-1"
-                />
-              ) : (
-                <div
-                  key={index}
-                  className={`bg-white rounded-lg  overflow-hidden ${
-                    index === 0
-                      ? 'lg:row-span-2 lg:col-span-1 md:row-span-1  col-span-1'
-                      : ''
-                  }`}
-                >
-                  <img
-                    src={index === 3 ? 'img/news_ima1.png' : news.imageUrl}
-                    alt=""
-                    className={`w-full   object-cover object-center rounded-lg  ${
-                      index === 0 ? 'md:h-auto' : 'md:max-h-[160px]'
-                    }
+      <section className="w-full flex flex-col justify-center items-center">
+        <div className="flex flex-col max-w-[1200px]">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 p-8">
+            Sự kiện nổi bật
+          </h1>
+          <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:grid-rows-2 gap-4 p-8 md:max-h-[600px]">
+            {newsData.map((news: any, index: number) => (
+              <>
+                {index === 1 ? (
+                  <ProjectCard
+                    key={index}
+                    project={news}
+                    className="lg:col-span-2 lg:row-span-2 md:col-span-1 md:row-span-1"
+                  />
+                ) : (
+                  <div
+                    key={index}
+                    className={`bg-white rounded-lg  overflow-hidden ${
+                      index === 0
+                        ? 'lg:row-span-2 lg:col-span-1 md:row-span-1  col-span-1'
+                        : ''
+                    }`}
+                  >
+                    <img
+                      src={index === 3 ? 'img/news_ima1.png' : news.imageUrl}
+                      alt=""
+                      className={`w-full   object-cover object-center rounded-lg  ${
+                        index === 0 ? 'md:h-auto' : 'md:max-h-[160px]'
+                      }
                    
                     `}
-                  />
-                  <div className="p-4">
-                    <h1
-                      className={`md:text-[15px] text-[15px] font-bold text-gray-800 ${index !== 0 && 'line-clamp-2'}`}
-                    >
-                      {news.title}
-                    </h1>
-                    {index === 0 && (
-                      <p className="text-gray-600 md:text-[15px] text-sm line-clamp-3 mt-6">
-                        Hyratek và Qualcomm hợp tác chiến lược về AI, đồng hành
-                        cùng dự án "Phục dựng ảnh liệt sĩ" của Hà Nội
-                      </p>
-                    )}
-
-                    <div
-                      className={`flex justify-between items-center ${
-                        index === 1 ? 'absolute' : ''
-                      }`}
-                    >
-                      <Link
-                        href={news.link || '#'}
-                        className="md:text-[15px] text-[#03A638] text-sm font-semibold"
+                    />
+                    <div className="p-4">
+                      <h1
+                        className={`md:text-[15px] text-[15px] font-bold text-gray-800 ${index !== 0 && 'line-clamp-2'}`}
                       >
-                        CÔNG NGHỆ
-                      </Link>
-                      <p className="text-sm text-gray-600 mt-2">{news.date}</p>
+                        {news.title}
+                      </h1>
+                      {index === 0 && (
+                        <p className="text-gray-600 md:text-[15px] text-sm line-clamp-3 mt-6">
+                          Hyratek và Qualcomm hợp tác chiến lược về AI, đồng
+                          hành cùng dự án "Phục dựng ảnh liệt sĩ" của Hà Nội
+                        </p>
+                      )}
+
+                      <div
+                        className={`flex justify-between items-center ${
+                          index === 1 ? 'absolute' : ''
+                        }`}
+                      >
+                        <Link
+                          href={news.link || '#'}
+                          className="md:text-[15px] text-[#03A638] text-sm font-semibold"
+                        >
+                          CÔNG NGHỆ
+                        </Link>
+                        <p className="text-sm text-gray-600 mt-2">
+                          {news.date}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )}
-            </>
-          ))}
+                )}
+              </>
+            ))}
+          </div>
         </div>
       </section>
 
       {/* upcomming */}
       <section className="w-full bg-cover bg-center flex justify-center flex-col items-center gap-3">
-        <h1 className="w-full text-left text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 px-8">
-          Sự kiện sắp tới
-        </h1>
+        <div className="max-w-[1200px] ">
+          <h1 className="w-full text-left text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 px-8">
+            Sự kiện sắp tới
+          </h1>
 
-        <div className="flex flex-col justify-center items-center gap-4 md:max-w-[80%]">
-          <img
-            className="h-auto max-w-[100%]"
-            src="/img/news/news_upcomming.png"
-            alt="image description"
-          />
-          <div className="container mx-auto md:max-w-[90%] overflow-x-auto">
-            <div className="flex gap-6 flex-nowrap">
-              {newsData.map((news: any, index: number) => (
-                <div
-                  key={index}
-                  className="bg-white rounded-lg shadow-lg overflow-hidden min-w-[250px] "
-                >
-                  <img
-                    src={news.imageUrl}
-                    alt=""
-                    className="w-full object-cover object-center bg-center max-h-[200px] h-auto"
-                  />
-                  <div className="p-4 ">
-                    <h1 className="md:text-[18px] text-[12px] font-bold text-gray-800">
-                      {news.title}
-                    </h1>
-                    <div className="flex justify-between items-center">
-                      <Link
-                        href={news.link || '#'}
-                        className="md:text-sm text-[#03A638] text-sm font-semibold"
-                      >
-                        CÔNG NGHỆ
-                      </Link>
-                      <p className="text-sm text-gray-600 text-center">
-                        {news.date}
-                      </p>
+          <div className="flex flex-col justify-center items-center gap-4">
+            <img
+              className="h-auto max-w-[100%]"
+              src="/img/news/news_upcomming.png"
+              alt="image description"
+            />
+            <div className="container mx-auto  overflow-x-auto">
+              <div className="flex gap-6 flex-nowrap">
+                {newsData.map((news: any, index: number) => (
+                  <div
+                    key={index}
+                    className="bg-white rounded-lg shadow-lg overflow-hidden min-w-[250px] "
+                  >
+                    <img
+                      src={news.imageUrl}
+                      alt=""
+                      className="w-full object-cover object-center bg-center max-h-[200px] h-auto"
+                    />
+                    <div className="p-4 ">
+                      <h1 className="md:text-[18px] text-[12px] font-bold text-gray-800">
+                        {news.title}
+                      </h1>
+                      <div className="flex justify-between items-center">
+                        <Link
+                          href={news.link || '#'}
+                          className="md:text-sm text-[#03A638] text-sm font-semibold"
+                        >
+                          CÔNG NGHỆ
+                        </Link>
+                        <p className="text-sm text-gray-600 text-center">
+                          {news.date}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
