bbc4638a32bf9fb7140b9c8e7c4a9288e47d0d96
Bang Vu <lenhkinhsa99@gmail.com>
2024-11-23 16:40:10 +0900

    update css for partner logo

--- src/app/partner.tsx
+++ src/app/partner.tsx
@@ -5,70 +5,71 @@ import Image from 'next/image';
 
 const ProjectGrid: React.FC = () => {
   return (
-    <Marquee autoFill className="bg-[#F3F7F4] py-[62px]">
+    <Marquee autoFill className="flex items-center bg-[#F3F7F4] py-[62px]">
       <Image
         width={257}
         height={82}
         src="/img/icon/coop1.png"
-        layout="responsive"
+        // layout="responsive"
         alt="Pindias"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
       <Image
         width={257}
         height={82}
         src="/img/icon/coop2.png"
-        layout="responsive"
+        // layout="responsive"
         alt="Vitala"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
-      <Image
-        width={257}
-        height={82}
-        src="/img/icon/coop3.png"
-        layout="responsive"
-        alt="Pencil"
-        className={`h-16 object-contain w-[200px] ${styles.partnerLogo}`}
-      />
+
       <Image
         width={257}
         height={82}
         src="/img/icon/coop4.png"
-        layout="responsive"
+        // layout="responsive"
         alt="Qualcomm"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
       <Image
         width={257}
         height={82}
         src="/img/icon/coop5.png"
-        layout="responsive"
+        // layout="responsive"
         alt="Certik"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
       <Image
         width={257}
         height={82}
         src="/img/icon/coop6.png"
-        layout="responsive"
+        // layout="responsive"
         alt="TaiPy"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+      />
+        <Image
+        width={257}
+        height={82}
+        src="/img/icon/coop3.png"
+        // layout="responsive"
+        alt="Pencil"
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
       <Image
         width={257}
         height={82}
         src="/img/icon/coop7.png"
-        layout="responsive"
+        // layout="responsive"
         alt="Eureka"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
       <Image
         width={257}
         height={82}
         src="/img/icon/coop8.png"
-        layout="responsive"
+        // layout="responsive"
         alt="Corex"
-        className={`h-16 object-contain ${styles.partnerLogo}`}
+        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
       />
     </Marquee>
   );
