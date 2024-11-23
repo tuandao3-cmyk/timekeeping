ffc2c7c21d78aced05a116c7851f40b9ac5fb6a8
Bang Vu <lenhkinhsa99@gmail.com>
2024-11-23 17:08:42 +0900

    update padding resp for partner

--- src/app/partner.tsx
+++ src/app/partner.tsx
@@ -5,14 +5,14 @@ import Image from 'next/image';
 
 const ProjectGrid: React.FC = () => {
   return (
-    <Marquee autoFill className="flex items-center bg-[#F3F7F4] py-[62px]">
+    <Marquee autoFill className="flex items-center bg-[#F3F7F4] py-[40px] lg:py-[62px]">
       <Image
         width={257}
         height={82}
         src="/img/icon/coop1.png"
         // layout="responsive"
         alt="Pindias"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
       <Image
         width={257}
@@ -20,7 +20,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop2.png"
         // layout="responsive"
         alt="Vitala"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
 
       <Image
@@ -29,7 +29,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop4.png"
         // layout="responsive"
         alt="Qualcomm"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
       <Image
         width={257}
@@ -37,7 +37,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop5.png"
         // layout="responsive"
         alt="Certik"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
       <Image
         width={257}
@@ -45,7 +45,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop6.png"
         // layout="responsive"
         alt="TaiPy"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
         <Image
         width={257}
@@ -53,7 +53,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop3.png"
         // layout="responsive"
         alt="Pencil"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
       <Image
         width={257}
@@ -61,7 +61,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop7.png"
         // layout="responsive"
         alt="Eureka"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
       <Image
         width={257}
@@ -69,7 +69,7 @@ const ProjectGrid: React.FC = () => {
         src="/img/icon/coop8.png"
         // layout="responsive"
         alt="Corex"
-        className={`h-16 object-contain w-auto max-w-[200px] px-4`}
+        className={`h-16 object-contain w-auto max-w-[224px] lg:max-w-[300px] px-[20px] lg:px-[50px]`}
       />
     </Marquee>
   );
